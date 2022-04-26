7dcdb362f84461721fa3398174b3ccbac59a4d60
itay koski <itaykoski1@gmail.com>
2022-04-26 22:24:19 +0300

    1231231

--- BitApp/BitApp.Android/Resources/Resource.designer.cs
+++ BitApp/BitApp.Android/Resources/Resource.designer.cs
@@ -16706,16 +16706,19 @@ namespace BitApp.Droid
 			public const int swapicon = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int test_custom_background = 2131165342;
+			public const int SwapM = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int tooltip_frame_dark = 2131165343;
+			public const int test_custom_background = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int tooltip_frame_light = 2131165344;
+			public const int tooltip_frame_dark = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int User = 2131165345;
+			public const int tooltip_frame_light = 2131165345;
+			
+			// aapt resource value: 0x7F0700A2
+			public const int User = 2131165346;
 			
 			static Drawable()
 			{
