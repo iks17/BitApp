226b3d0a860850052dff223536cd8dc6d5e99326
itay koski <itaykoski1@gmail.com>
2022-04-26 22:51:56 +0300

    123123

--- BitApp/BitApp/App.xaml.cs
+++ BitApp/BitApp/App.xaml.cs
@@ -23,7 +23,7 @@ namespace BitApp
             InitializeComponent();
 
 
-            MainPage = new NavigationPage(new SwapPage());
+            MainPage = new NavigationPage(new DashboardPage());
         }
         
 
